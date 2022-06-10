c9b4976b0478a8e005dd7014c9b54eeff3a161a8
dahachoi <106780741+dahachoi@users.noreply.github.com>
2022-06-10 00:52:23 -0700

    made ghost abstract class

--- PacMan/CGhost.h
+++ PacMan/CGhost.h
@@ -7,9 +7,24 @@ enum class	BEHAVIOUR{SCATTER = 0, CHASE, FRIGHTENED, EATEN};
 class CGhost
 {
 public:
-	void Update();
-	void Render(sf::RenderTarget*);
+	virtual void Update() = 0;
+	virtual void Render(sf::RenderTarget*) = 0;
 private:
 
+	BEHAVIOUR mState = BEHAVIOUR::SCATTER;
+	std::string mName;
+
+	//Sprite
+	sf::Sprite mGhost;
+	sf::Texture mTexture;
+
+	float mX;
+	float mY;
+	float mMovementSpeed = 1.f;
+
+	virtual void ScatterState() = 0;
+	virtual void ChaseState() = 0;
+	void FrightenedState();
+	void EatenState();
 };
 
