32aa7020f3b4e103850cd551ff14c9023afb0e85
dahachoi <106780741+dahachoi@users.noreply.github.com>
2022-06-09 17:19:49 -0700

    updated side collision

--- PacMan/CGame.cpp
+++ PacMan/CGame.cpp
@@ -46,8 +46,28 @@ void CGame::UpdatePacMan() {
 
 void CGame::UpdateCollision() {
 	sf::Vector2f pacManPos = iPacMan.GetGridCoordinate();
+	sf::CircleShape mPacMan = iPacMan.GetShape();
 	float pacManX = pacManPos.x;
 	float pacManY = pacManPos.y;
+	const float pacManWidth = 44.f;
+
+	//side of screen
+	if (pacManX <= 0) {
+		//if (pacManX == 0) {
+		//	iCoins.UpdateCollision(pacManX, pacManY);
+		//}
+
+		if (pacManX == -pacManWidth * 2 && pacManY == 17.f * 27.f) {
+			iPacMan.SetPosition(float(WIDTH) + pacManWidth, pacManY + 13.f);
+		}
+		return;
+	}
+	else if (pacManX >= WIDTH) {
+		if (pacManX == WIDTH + pacManWidth && pacManY == 17.f * 27.f) {
+			iPacMan.SetPosition((13 - pacManWidth) * 2, pacManY + 13.f);
+		}
+		return;
+	}
 
 	// Collision with wall
 	if (fmod(pacManX, 27) == 0.0 && fmod(pacManY, 27) == 0.0 || iPacMan.IsStopped()) {
@@ -58,6 +78,7 @@ void CGame::UpdateCollision() {
 			iPacMan.StopPacMan();
 		}
 	}
+	cout << endl;
 }
 
 //Render
